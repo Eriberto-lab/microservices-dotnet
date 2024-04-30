cc6abf60a990b12453e4c0441730b1b451e0b317
Eriberto Lima <eribertosflima@gmail.com>
2024-04-30 15:10:02 -0300

    feat: add config do automapper

--- GeekShooping/GeekShopping.ProductAPI/Program.cs
+++ GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +13,11 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
     connection,
     new MySqlServerVersion(new Version(8, 3, 0)
     )));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- GeekShooping/GeekShopping.ProductAPI/obj/Debug/net8.0/GeekShopping.ProductAPI.AssemblyInfo.cs
+++ GeekShooping/GeekShopping.ProductAPI/obj/Debug/net8.0/GeekShopping.ProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad771f393ebc78115a2e31418ebcd2a8e6f9a589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870bab4829594c5014d4cd76a4d9e58386e8cfcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
